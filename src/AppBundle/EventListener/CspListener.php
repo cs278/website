a4ecc44b29b6c3a8d544d23d06a593d2e5dc74bc
Chris Smith <chris@cs278.org>
2016-01-11 23:03:20 +0000

    Add CSP in production

---
+++ src/AppBundle/EventListener/CspListener.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+final class CspListener implements EventSubscriberInterface
+{
+    private $policy;
+    private $enforce;
+    private $reportUri;
+
+    public function __construct($policy, $enforce = false, $reportUri = null)
+    {
+        $this->policy = $policy;
+        $this->enforce = (bool) $enforce;
+        $this->reportUri = $reportUri;
+    }
+
+    public function onKernelResponse(FilterResponseEvent $e)
+    {
+        if (!$e->isMasterRequest()) {
+            return;
+        }
+
+        $response = $e->getResponse();
+
+        if ($response->isRedirect()) {
+            return;
+        }
+
+        $headerName = $this->enforce
+            ? 'Content-Security-Policy'
+            : 'Content-Security-Policy-Report-Only';
+
+        if ($this->reportUri) {
+            $headerValue = '%1$s report-uri %2$s;';
+        } else {
+            $headerValue = '%1$s';
+        }
+
+        $headerValue = sprintf($headerValue, $this->policy, $this->reportUri);
+
+        $response->headers->set($headerName, $headerValue);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        ];
+    }
+}
