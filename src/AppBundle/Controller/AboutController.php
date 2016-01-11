a2d519656f2e94a312fbd60a95cb773d88732e80
Chris Smith <chris@cs278.org>
2016-01-11 00:39:58 +0000

    Import about page and related stuff from legacy website

---
+++ src/AppBundle/Controller/AboutController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\EngineInterface;
+
+/**
+ * @Route(service="app.about_controller")
+ */
+final class AboutController
+{
+    private $templating;
+
+    public function __construct(EngineInterface $templating)
+    {
+        $this->templating = $templating;
+    }
+
+    /**
+     * @Route("/", name="about")
+     */
+    public function indexAction(Request $request)
+    {
+        return new Response(
+            $this->templating->render('about/index.html.twig'),
+            Response::HTTP_OK
+        );
+    }
+}
