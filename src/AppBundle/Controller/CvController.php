faffbe09e75fa94f1553a96f46fc1691990ec5f4
Chris Smith <chris@cs278.org>
2016-01-15 00:20:36 +0000

    Further work on CV

--- src/AppBundle/Controller/CvController.php
+++ src/AppBundle/Controller/CvController.php
@@ -28,6 +28,37 @@ final class CvController
     {
         $cv = json_decode(file_get_contents($this->jsonFile), true);
 
+        // Convert dates into DateTime objects.
+        array_walk_recursive($cv, function (&$value, $key) {
+            if ($key === 'startDate' || $key === 'endDate') {
+                $value = \DateTime::createFromFormat('Y-m-d', $value);
+            }
+        });
+
+        if (true) {
+            // Apply sanitisation.
+            unset($cv['basics']['phone']);
+            unset($cv['basics']['location']['address']);
+            unset($cv['basics']['location']['postalCode']);
+
+            $cv['references'] = [];
+        }
+
+        $profiles = [];
+
+        foreach ($cv['basics']['profiles'] as $profile) {
+            $network = mb_strtolower($profile['network']);
+            // Remove non letters/numbers Unicode aware of course.
+            $network = preg_replace('{[^\p{L}\p{N}]}', '', $network);
+
+            $profiles[$network] = [
+                'username' => $profile['username'],
+                'url' => $profile['url'],
+            ];
+        }
+
+        $cv['profiles'] = $profiles;
+
         return new Response(
             $this->templating->render('about/cv.html.twig', $cv),
             Response::HTTP_OK
