54c5553349856f76f7151e47b2a1335d37d534b8
Chris Smith <chris@cs278.org>
2016-01-17 16:28:02 +0000

    Temp commit for testing composer

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 final class DefaultController extends Controller
 {
@@ -14,5 +15,13 @@ final class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("redirect")
+     */
+    public function redirectAction(Request $request)
+    {
+        return new RedirectResponse('https://www.google.com/robots.txt');
     }
 }
