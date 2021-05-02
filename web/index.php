d4ef5e4a450eae62af72aa708b3c8d1f5627db1f
Chris Smith <chris@cs278.org>
2021-05-02 01:11:21 +0100

    Fix front controller

--- web/index.php
+++ web/index.php
@@ -4,15 +4,16 @@ use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 // Set a default error response
-// http_response_code(500);
+http_response_code(500);
 
 require __DIR__.'/../vendor/autoload.php';
 
-$env = $_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? 'dev';
+$env = $_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? '';
+$env = $env !== '' ? $env : 'dev';
 $debug = filter_var($_ENV['APP_DEBUG'] ?? $_SERVER['APP_DEBUG'] ?? ('dev' === $env), FILTER_VALIDATE_BOOL);
 
 if ($debug) {
-    // Debug::enable();
+    Debug::enable();
 }
 
 $kernel = new AppKernel($env, $debug);
