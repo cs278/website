33c3df3e008346872385c6f5c23e76bcb895ce9b
Chris Smith <chris@cs278.org>
2021-05-02 17:04:20 +0100

    Handle some quirks of fly.io

---
+++ src/prepend.php
@@ -0,0 +1,14 @@
+<?php
+
+foreach ($_ENV as $name => $value) {
+    if ($value === '') {
+        unset($_ENV[$name]);
+        unset($_SERVER[$name]);
+    }
+}
+
+if (($_ENV['HTTP_FLY_CLIENT_IP'] ?? '') !== '' && ($_ENV['FLY_APP_NAME'] ?? '') !== '') {
+    $_ENV['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'] = $_ENV['HTTP_FLY_CLIENT_IP'];
+    $_ENV['REMOTE_HOST'] = $_SERVER['REMOTE_HOST'] = $_ENV['HTTP_FLY_CLIENT_IP'];
+    unset($_ENV['HTTP_FLY_CLIENT_IP'], $_SERVER['HTTP_FLY_CLIENT_IP']);
+}
